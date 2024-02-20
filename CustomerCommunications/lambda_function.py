f4e15bcbb6a77de69f17d0e75dcfe3fb1587bfb9
pratheekshavrao <156860208+pratheekshavrao@users.noreply.github.com>
2024-02-20 16:21:34 -0700

    Update lambda_function.py

--- CustomerCommunications/lambda_function.py
+++ CustomerCommunications/lambda_function.py
@@ -12,8 +12,8 @@ rds_client = boto3.client('rds-data')
 
 #database declarations
 database_name = 'orders_management_aurora'
-secret_store_arn = 'arn:aws:secretsmanager:us-east-1:471376517949:secret:rds-db-credentials/cluster-KBPJLBNDMCIOMAYSASMPW6IHSI/test_username/1706120307628-3FB3ed'
-db_cluster_arn = 'arn:aws:rds:us-east-1:471376517949:cluster:orders-db-cluster'
+secret_store_arn = 'Insert ARN here'
+db_cluster_arn = 'Insert ARN here'
 
 
 #SES declarations
